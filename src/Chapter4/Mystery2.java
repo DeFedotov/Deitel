fb1b6fbe5319f5ab6b2b3881813f635d3ecc5e6e
Denis Fedotov <fd.postbox@gmail.com>
2020-06-04 09:34:36 +0300

    Ex.4.35

---
+++ src/Chapter4/Mystery2.java
@@ -0,0 +1,15 @@
+package Chapter4;
+
+//4.25 What does the following program print?
+
+public class Mystery2 {
+	public static void main(String[] args) {
+
+		int count = 1;
+
+		while (count <= 10){
+			System.out.println(count % 2 == 1 ? "****" : "++++++++");
+			++count;
+		}
+	}
+}
