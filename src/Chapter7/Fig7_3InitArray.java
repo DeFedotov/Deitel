e840b4f61d320f3394b90108bd637b19dadda03c
Denis Fedotov <fd.postbox@gmail.com>
2020-09-21 17:14:51 +0300

    Ex.7_3

---
+++ src/Chapter7/Fig7_3InitArray.java
@@ -0,0 +1,17 @@
+package Chapter7;
+
+// Initializing the elements of an array with an array initializer.
+
+public class Fig7_3InitArray {
+
+	public static void main(String[] args) {
+
+		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+
+		System.out.printf("%s%8s%n", "Index", "Value");
+
+		for (int counter = 0; counter < array.length; counter++){
+			System.out.printf("%5d%8d%n", counter, array[counter]);
+		}
+	}
+}
