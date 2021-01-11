adf77364898f6ac0ec829f34794b99b90a84768c
Denis Fedotov <fd.postbox@gmail.com>
2021-01-11 23:31:55 +0300

    Fig. 2.15

---
+++ src/Chapter2/Comparison.java
@@ -0,0 +1,40 @@
+package Chapter2;
+
+//Fig 2.15
+
+import java.util.Scanner;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int number1;
+		int number2;
+
+		System.out.print("Enter first integer: ");
+		number1 = input.nextInt();
+
+		System.out.print("Enter second integer: ");
+		number2 = input.nextInt();
+
+		if (number1 == number2)
+			System.out.printf("%d == %d%n", number1, number2);
+
+		if (number1 != number2)
+			System.out.printf("%d != %d%n", number1, number2);
+
+		if (number1 < number2)
+			System.out.printf("%d < %d%n", number1, number2);
+
+		if (number1 > number2)
+			System.out.printf("%d > %d%n", number1, number2);
+
+		if (number1 >= number2)
+			System.out.printf("%d >= %d%n", number1, number2);
+
+		if (number1 <= number2)
+			System.out.printf("%d <= %d%n", number1, number2);
+	}
+}
